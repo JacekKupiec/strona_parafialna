7c6541bbc0a6b5f95a6f35f889a3d558cceef1aa
JacekKupiec <jacek.kupiec@o2.pl>
2014-08-11 21:16:07 +0200

    To jest pierwszy komit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Parafia NMP Matki Kościoła i św. Katarzyny Szweckiej</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="assets/css/application.css" />
+    </head>
+    <body>
+        <div class="banner">
+            <img src="assets/images/application/Baner przeskalowany.jpg" alt="banner" />  
+        </div>
+        
+        <div class="application-container">
+            <div class="title">
+                <h1>Parafia NMP Matki Kościoła i św. Katarzyny Szweckiej</h1>
+            </div>
+            <div class="menu">
+                <div class="dropdown-list-tab">
+                    <a>Parafia</a>
+                    <ul>
+                        <li><a>Historia</a></li>
+                        <li>Kontakt</li>
+                        <li>
+                            <a>Duszpasterze</a>
+                            <ul>
+                                <li>Tadeusz Balicki</li>
+                                <li>Piotr Przyborek</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Ogłoszenia parafialne</a>
+                            <ul>
+                                <li>Ogłoszenia bierzące</li>
+                                <li>Archiwum</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="dropdown-list-tab">
+                    <a>Grupy duszpasterskie</a>
+                    <ul>
+                        <li><a>Rada parafialna</a></li>
+                        <li><a>Ucho Igielne</a></li>
+                        <li><a>Służba liturgiczna</a></li>
+                        <li><a>Semper fidelis</a></li>
+                        <li><a>Żywy Różaniec</a></li>
+                    </ul> 
+                </div>
+                <div class="dropdown-list-tab">
+                    <a>Galeria zdjęć</a>
+                    <ul>
+                        <li>Szczyrk 2014</li>
+                        <li> Pożegnanie księdza proboszcza</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="main-content">
+                Ogólnie znana teza głosi, iż użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce 
+                zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych 
+                „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, 
+                sprawiającego, że wygląda to „zbyt czytelnie” po polsku. Wielu webmasterów i designerów używa Lorem 
+                Ipsum jako domyślnego modelu tekstu i wpisanie w internetowej wyszukiwarce ‘lorem ipsum’ spowoduje 
+                znalezienie bardzo wielu stron, które wciąż są w budowie. Wiele wersji tekstu ewoluowało i zmieniało 
+                się przez lata, czasem przez przypadek, czasem specjalnie (humorystyczne wstawki itd).
+                W przeciwieństwie do rozpowszechnionych opinii, Lorem Ipsum nie jest tylko przypadkowym tekstem. Ma 
+                ono korzenie w klasycznej łacińskiej literaturze z 45 roku przed Chrystusem, czyli ponad 2000 lat temu! 
+                Richard McClintock, wykładowca łaciny na uniwersytecie Hampden-Sydney w Virginii, przyjrzał się uważniej 
+                jednemu z najbardziej niejasnych słów w Lorem Ipsum – consectetur – i po wielu poszukiwaniach odnalazł 
+                niezaprzeczalne źródło: Lorem Ipsum pochodzi z fragmentów (1.10.32 i 1.10.33) „de Finibus Bonorum et Malorum”, 
+                czyli „O granicy dobra i zła”, napisanej właśnie w 45 p.n.e. przez Cycerona. Jest to bardzo popularna w czasach 
+                renesansu rozprawa na temat etyki. Pierwszy wiersz Lorem Ipsum, „Lorem ipsum dolor sit amet...” pochodzi 
+                właśnie z sekcji 1.10.32.
+            </div>
+            <div class="footer" >Stopka</div>
+        </div>
+    </body>
+</html>

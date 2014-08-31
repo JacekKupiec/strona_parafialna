5758c7b6ea5ffa215e71b99db2bda25e6d89e807
Jacek Kupiec <jacek.kupiec@o2.pl>
2014-08-31 11:30:07 +0200

    Dobra struktura, rozszerzenie koncepcj ogłoszeń na posty, teraz będze łatwiej

--- databases/tabela_ogloszenia.sql
+++ databases/tabela_ogloszenia.sql
@@ -1,5 +1,13 @@
-CREATE Table IF NOT EXISTS ogloszenia (
+CREATE Table IF NOT EXISTS posty ( 
+/*Ze względu na to ,że ogłoszenia będę przechowywane w 
+takiej samej formie jak aktualności na stronie głównej to
+jest do tego 1 tabela, ale z dodatkowym polem typ,
+jak na razie będą to ogloszenia, i aktualnosci, potem może się coś doda ;),
+jak choćby posty do forum*/
 	id int auto_increment primary key,
 	data_publikacji date NOT NULL,
+	typ varchar(255) NOT NULL,
 	tresc text NOT NULL
 ) ENGINE = InnoDB;
+
+CREATE INDEX index_ogloszenia_typ ON ogloszenia(typ);

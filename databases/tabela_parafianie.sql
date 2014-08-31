6c472f5435bed6c7c5c1c335b15c08338250e86a
Jacek Kupiec <jacek.kupiec@o2.pl>
2014-08-31 11:16:02 +0200

    Dobra struktura bazy danych, unikalne dane logowania

--- databases/tabela_parafianie.sql
+++ databases/tabela_parafianie.sql
@@ -2,7 +2,7 @@ CREATE Table IF NOT EXISTS parafianie (
 	id int auto_increment primary key,
 	imie varchar(32) NOT NULL,
 	nazwisko varchar(64) NOT NULL,
-	dane_logowania int,
+	dane_logowania int UNIQUE,
 	id_adresu int,
 	nr_telefonu int(9),
 	id_zdjecia int UNIQUE, /* Nie może byc przecież tak ,że 2 osoby mają to samo zdjęcie, jedna z nich kłamie*/
